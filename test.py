afbe770740c5fe6e768c7e91f441a088838e0ea4
Almaz Shakirov <almazshakirov@Almazs-MacBook-Pro.local>
2024-01-25 20:58:57 +0300

    task1

---
+++ test.py
@@ -0,0 +1,13 @@
+from math import sum
+
+
+def test(a, b):
+    actual = sum(a, b)
+    expected = a + b
+    assert expected == actual
+
+
+test(-5, 0)
+test(-10, 21)
+test(1000, 6781)
+test(9000, 6781)
